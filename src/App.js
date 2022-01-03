ac661092ab4d10dbbd381a321360c02b6ec99c04
nikeshvpatel <ptlnik13@gmail.com>
2022-01-03 13:12:35 -0500

    83. Adding collection Items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import {Route} from "react-router-dom";
 function App() {
   return (
    <div>
        <Route exact path='/' component={HomePage}/>
+       <Route path='/shop' component={ShopPage}/>
    </div>
   );
 }
